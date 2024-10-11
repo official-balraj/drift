f714e2aaced3c8ed8abb497362b6b2382920bf1c
Balraj <balraj@192.168.1.6>
2024-10-11 22:49:24 +0530

    added backend spring structure of application

---
+++ services/drift-service/src/main/java/com/service/Drift/DriftApplication.java
@@ -0,0 +1,13 @@
+package com.service.Drift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriftApplication.class, args);
+	}
+
+}
---
+++ services/drift-service/src/test/java/com/service/Drift/DriftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.Drift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
